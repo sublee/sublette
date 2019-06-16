c2a1b16bf4181bb3f148d4fd6513137df67cb759
Heungsub Lee <sub@subl.ee>
2019-06-16 18:19:02 +0900

    Support downgraded Pillow

--- factory/plates.py
+++ factory/plates.py
@@ -13,7 +13,8 @@ from sublette import sublette
 HERE = os.path.dirname(__file__)
 OUTPUT_PATTERN = os.path.join(HERE, '../plates/%s.gif')
 
-for name, hex in sublette.items():
+for name, color in sublette.items():
     output = OUTPUT_PATTERN % name.lower().replace(' ', '-')
-    img = Image.new('P', (18, 18), hex)
+    img = Image.new('RGB', (18, 18), color)
+    img.convert('P')
     img.save(output)
