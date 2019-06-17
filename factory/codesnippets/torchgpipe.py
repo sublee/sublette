a8524d37ead1b6bfb5a9f32670c83f7de2f1b1fa
Heungsub Lee <sub@subl.ee>
2019-06-18 00:19:47 +0900

    Python code snippet includes comments

--- factory/codesnippets/torchgpipe.py
+++ factory/codesnippets/torchgpipe.py
@@ -1,6 +1,9 @@
 from torchgpipe import GPipe
 
+# Prepare a sequential model.
 model = nn.Sequential(a, b, c, d)
+
+# Wrap your model with GPipe.
 model = GPipe(model, balance=[1, 1, 1, 1], chunks=8)
 
 for input in data_loader:
