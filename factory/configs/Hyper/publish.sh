d7b0d2ad1892006b47432391f9746c307d6df1f9
Heungsub Lee <sub@subl.ee>
2019-06-19 20:55:47 +0900

    Publish hyper-sublette to NPM

---
+++ factory/configs/Hyper/publish.sh
@@ -0,0 +1,11 @@
+# Publish hyper-sublette to NPM.
+PREV_VERSION="$(npm show hyper-sublette version 2>/dev/null || echo 0.0.0)"
+
+MAJOR="$(echo "$PREV_VERSION" | awk -F. '{ print $1 }')"
+MINOR="$(echo "$PREV_VERSION" | awk -F. '{ print $2 }')"
+PATCH="$(echo "$PREV_VERSION" | awk -F. '{ print $3 }')"
+
+VERSION="$MAJOR.$MINOR.$((PATCH+1))"
+sed -i "s/#VERSION#/$VERSION/g" package.json
+
+npm publish
